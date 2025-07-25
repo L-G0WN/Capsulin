2877df3b35feade52a7cc3cee0bedc4afbd3f461
Héctor Monagas <monagasrr@gmail.com>
2025-07-25 11:15:19 -0400

    Fixed relations symptos

--- src/pages/api/list/medicamentos.ts
+++ src/pages/api/list/medicamentos.ts
@@ -1,15 +1,27 @@
 import { getDb } from "@/lib/db.ts";
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async () => {
+export const GET: APIRoute = async ({ url }) => {
     const db = await getDb();
-    const medicamentos = await db.all(`
-        SELECT m.id, m.nombre
-        FROM medicamentos m
-        LEFT JOIN sintoma_medicamento sm ON sm.medicamento_id = m.id
-        WHERE sm.medicamento_id IS NULL
-        ORDER BY m.nombre ASC
-    `);
+    const sintomaId = url.searchParams.get("sintomaId");
+    let medicamentos;
+    if (sintomaId) {
+        medicamentos = await db.all(`
+            SELECT m.id, m.nombre, m.presentacion_id, p.nombre AS presentacion
+            FROM medicamentos m
+            JOIN presentaciones p ON m.presentacion_id = p.id
+            WHERE m.id NOT IN (
+                SELECT medicamento_id FROM sintoma_medicamento WHERE sintoma_id = ?
+            )
+            ORDER BY m.nombre ASC
+        `, sintomaId);
+    } else {
+        medicamentos = await db.all(`
+            SELECT m.id, m.nombre
+            FROM medicamentos m
+            ORDER BY m.nombre ASC
+        `);
+    }
     await db.close();
     return new Response(JSON.stringify(medicamentos), { status: 200 });
 };
